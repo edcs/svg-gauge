5c6f6f21d07021f7cb4e8695d3c71eb9f3f1ca73
Edward Coleridge Smith <edward.smith@pod-point.com>
2016-09-04 17:06:10 +0100

    Setup views

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const style = {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    backgroundColor: '#4D4D4D',
+};
+
+const App = () => (
+    <div style={style}>
+    </div>
+);
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+import App from './containers/App';
 import configureStore from './state/store';
 
 const store = configureStore();
 
 ReactDOM.render(
     <Provider store={store}>
-        <div />
+        <App />
     </Provider>,
     document.getElementById('root')
 );
